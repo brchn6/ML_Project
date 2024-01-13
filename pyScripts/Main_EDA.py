475f7d94a344961bb09f3dd90521c2c42b58a8c8
barc <barcooooo>
2024-01-13 15:51:58 +0200

    propertice

--- pyScripts/Main_EDA.py
+++ pyScripts/Main_EDA.py
@@ -34,42 +34,71 @@ sns.set_style("darkgrid")
 plt.style.use("dark_background")
 
 #%% SeeTheData script OOP will be use in the future
-# a= SeeTheData(Maindf)
-# a.Subsetting()
+a= SeeTheData(df)
+a.Subsetting()
 # a.Display()
-# a.CountPlotOfObjectColumns()
+a.CountPlotOfObjectColumns()
 # a.HistPlotOfNumericColumns()
 #%%
 df = Maindf
 #Removing non-diabetes diagnosis should be before starting EDA
 Subset_df = df[df['diag_1'].str.contains('250') | df['diag_2'].str.contains('250') | df['diag_3'].str.contains('250')]
 df = Subset_df
+df.loc[df["readmitted"] == ">30" , "readmitted"] = "NO"
+df["categoricalValue"] = df["insulin"]
+df= df.reset_indexindex()
 # %%
 #display all data:
 def display_all(data):
     with pd.option_context("display.max_row", 100, "display.max_columns", 100):
         display(data)
-#%%
-np.random.seed(42)
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
-train_set, test_set = split_train_test(df, 0.2)
-print(len(train_set))
-print(len(test_set))
 
 #%%
+
+#%%
+from zlib import crc32
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+import hashlib
+
+def test_set_check(identifier, test_ratio, hash=hashlib.md5):
+    return bytearray(hash(np.int64(identifier)).digest())[-1] < 256 * test_ratio
+
+ColName= "insulin"
+
+df_with_id = df.reset_index()   # adds an `index` column
+train_set, test_set = split_train_test_by_id(df_with_id, 0.2, "index")
+
 from sklearn.model_selection import StratifiedShuffleSplit
 
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(df, df["readmitted"]):
+for train_index, test_index in split.split(df, df[ColName]):
     strat_train_set = df.loc[train_index]
     strat_test_set = df.loc[test_index]
 
-strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+def income_cat_proportions(data):
+    return data["categoricalValue"].value_counts() / len(data)
+
+
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+compare_props = pd.DataFrame({
+    "Overall": income_cat_proportions(df),
+    "Stratified": income_cat_proportions(strat_test_set),
+    "Random": income_cat_proportions(test_set),
+}).sort_index()
+compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
+compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+compare_props
 
 #%%
 display_all(df.info())
