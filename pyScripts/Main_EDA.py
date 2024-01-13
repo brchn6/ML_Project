fbbf258205ce5f4d775794edbeab82a01a06ad32
barc <barcooooo>
2024-01-13 16:03:19 +0200

    ..OWOW

--- pyScripts/Main_EDA.py
+++ pyScripts/Main_EDA.py
@@ -54,21 +54,6 @@ def display_all(data):
         display(data)
 
 #%%
-from zlib import crc32
-
-def test_set_check(identifier, test_ratio):
-    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
-
-def split_train_test_by_id(data, test_ratio, id_column):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]
-
-import hashlib
-
-def test_set_check(identifier, test_ratio, hash=hashlib.md5):
-    return bytearray(hash(np.int64(identifier)).digest())[-1] < 256 * test_ratio
-
 ColName= "insulin"
 
 df_with_id = df.reset_index()   # adds an `index` column
@@ -83,7 +68,6 @@ for train_index, test_index in split.split(df, df[ColName]):
 
 def income_cat_proportions(data):
     return data["categoricalValue"].value_counts() / len(data)
-
 
 from sklearn.model_selection import train_test_split
 
