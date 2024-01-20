c71c8a9e0159d03a6529112d7a07c02106e3e508
barc <barcooooo>
2024-01-20 12:32:45 +0200

    discharge_disposition_id_DF

--- pyScripts/ScriptNumberTwo.py
+++ pyScripts/ScriptNumberTwo.py
@@ -18,8 +18,8 @@ pd.set_option("display.max_row", 100) #add a option of pd
 pd.set_option("display.max_columns", 100) #add a option of pd
 
 from ScriptNumberOne import train_set
+from ScriptNumberOne import Mapdf
 ################################################################################################################################################################
-
 def drop_duplicates_fromDF(df,subset_col):
    NumberOf_patient_nbr_substract= df.shape[0] - df.drop_duplicates(subset=subset_col, keep="first").shape[0]
    df= df.drop_duplicates(subset=subset_col, keep="first")
@@ -30,3 +30,11 @@ def drop_duplicates_fromDF(df,subset_col):
 # dim is [25727 rows x 51 columns]
 #it was >>> train_set.shape (30419, 51)
 train_set = drop_duplicates_fromDF(train_set,"patient_nbr") [0]
+################################################################################################################################################################
+#from now ion the train set is as follw: its because we drop akk the dead papule
+# dim is [25468 rows x 51 columns]
+#it was >>> [25727 rows x 51 columns]
+discharge_disposition_id_DF = Mapdf[["discharge_disposition_id", "description.1"]]
+discharge_disposition_id_DF = discharge_disposition_id_DF[discharge_disposition_id_DF['description.1'].str.contains('Hospice') | discharge_disposition_id_DF['description.1'].str.contains('Expired')]
+train_set[~train_set['discharge_disposition_id'].isin(discharge_disposition_id_DF ['discharge_disposition_id'])]
+################################################################################################################################################################
