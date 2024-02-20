f0a445b48e6bd359d178c14e6bf83e0d22055932
guyil <guyilan1gu@gmail.com>
2024-02-20 18:53:57 +0200

    Completed training table

--- pyScripts/test_models.py
+++ pyScripts/test_models.py
@@ -1,4 +1,4 @@
-#%%
+
 # ------------------------------ code ------------------------------ #
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
@@ -43,7 +43,7 @@ classifiers = [rnd_clf, xgb_clf, lgbm_clf, catboost_clf, logistic_reg]
 # Define the scoring metrics (perofrmance measure (pm))
 scoring = {
     'neg_log_loss': make_scorer(log_loss, greater_is_better=False, needs_proba=True),
-    'accuracy': 'accuracy',
+    'accuracy': make_scorer(accuracy_score),
     'precision': make_scorer(precision_score, average='weighted'),
     'recall': make_scorer(recall_score, average='weighted'),
     'f1_weighted': make_scorer(f1_score, average='weighted'),
@@ -66,6 +66,35 @@ for classifier in classifiers:
             cv_scores[classifier_name] = {}
         cv_scores[classifier_name][s] = cross_val_scores
 
+model_scores = {}
+
+for classifier in classifiers:
+    classifier_name = type(classifier).__name__
+    model_scores[classifier_name] = {}
+
+    # Fit the classifier to the data
+    classifier.fit(X, y_encoded)
+
+    # Predict probabilities for log loss and roc_auc
+    if hasattr(classifier, "predict_proba"):
+        y_pred_proba = classifier.predict_proba(X)
+    else:
+        y_pred_proba = None
+
+    # Calculate scores for each metric
+    for s in scoring:
+        if s == 'neg_log_loss' or s == 'roc_auc':
+            if y_pred_proba is not None:
+                score = scoring[s](classifier, X, y_encoded)
+            else:
+                score = None
+        else:
+            y_pred = classifier.predict(X)
+            score_func = scoring[s]
+            score = score_func._score_func(y_encoded, y_pred)
+
+        model_scores[classifier_name][s] = score
+
 # Now, cv_scores dictionary contains cross-validation scores for each classifier and each scoring metric
 # You can analyze or further process these scores as needed
 
@@ -76,20 +105,23 @@ models_test_table = pd.DataFrame()
 
 for classifier in cv_scores.keys():
     for pm in cv_scores[classifier].keys():
-        models_test_table.loc[classifier,pm] = cv_scores[classifier][pm].mean()
+        models_test_table.loc[classifier,(pm+'-cv')] = cv_scores[classifier][pm].mean()
+
+for classifier in model_scores.keys():
+    for pm in model_scores[classifier].keys():
+        models_test_table.loc[classifier,pm] = model_scores[classifier][pm]
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
-# %%
+
 #data output as tbl
 import pandas as pd
 df = models_test_table
-df.to_csv('cv_scores.csv', index=True)
+df.to_csv('cv_scores_full.csv', index=True)
 
 #%%
-cv_df = pd.read_csv('cv_scores.csv').set_index('Unnamed: 0').rename_axis('Classifier')
+cv_df = pd.read_csv('cv_scores_full.csv').set_index('Unnamed: 0').rename_axis('Classifier')
 # ------------------------------ end ------------------------------ #
 
 
-# %%
