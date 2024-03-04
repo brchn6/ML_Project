147b8be5b386e50a4733eb58ee0b0a4878b9888c
barc <barcooooo>
2024-03-04 18:25:37 +0200

    הרבה עבודה

--- pyScripts/RunPipe.py
+++ pyScripts/RunPipe.py
@@ -23,7 +23,6 @@ preprocessing = make_pipeline(
     A1CTransformer(),
     CustomTransformer(functions),
 )
-
 #---------------------------------get the X train and y train--------------------------------
 #in a df mode not a numpy array
 df = preprocessing.fit_transform(train_set)
@@ -36,8 +35,28 @@ df = preprocessing.transform(test_set)
 X_test = df.drop("readmitted", axis=1)
 y_test = df["readmitted"].copy()
 
-#%%
+"""
+In order to implament the data into ML model, we need to convert the data into
+data that can goes into onehotencoder and standard scaler for the categorical and
+standard scaler for the numerical data
+to do that we will use the col_processor pipeline which we wrote in the DefPipeLineClasses.py
+on the X_train and y_train to get the X_train_np and y_train_np
+"""
 # --------------------------------- get the X train and y train --------------------------------
 # in a numpy array mode not a df
 X_train_np = col_processor.fit_transform(X_train)
-y_train_np = y_train.to_numpy()
+X_test_np = col_processor.transform(X_test)
+
+# if we wish to see the data in a df mode we can use the following code
+def show_data():
+    print("X_train_np:")
+    display(pd.DataFrame(X_train_np.toarray()))
+    print("X_test_np:")
+    display(pd.DataFrame(X_test_np.toarray()))
+
+
+def main():
+    show_data()
+
+#to call the main function remove the comment from the next line
+# main()
