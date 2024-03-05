58ee3562cc3772cc46a4cf58fe7fceecb5de0bb3
barc <barcooooo>
2024-03-05 18:13:00 +0200

    Refactor preprocessing pipeline in RunPipe.py

--- pyScripts/RunPipe.py
+++ pyScripts/RunPipe.py
@@ -5,16 +5,13 @@ the output of this script is the transformed data
 """
 #---------------------------------Imports--------------------------------
 import pandas as pd
-
 from DefPipeLineClasses import *
 from prepare_data import *
 import matplotlib.pyplot as plt
 from sklearn.compose import make_column_selector as selector
 #---------------------------------getting the data--------------------------------
 train_set, test_set ,Mapdf= prepare_data_main()
-
 #---------------------------------and preprocessing pipeline--------------------------------
-
 preprocessing = make_pipeline(
     DropDup(dropdup_col),
     DropColumns(columns_to_drop),
