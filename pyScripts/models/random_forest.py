e28ce2fac304b9cbf1b246ea194c8eaec6876438
barc <barcooooo>
2024-03-06 13:32:07 +0200

    httkv

--- pyScripts/models/random_forest.py
+++ pyScripts/models/random_forest.py
@@ -1,3 +1,4 @@
+#%%
 '''
 This script is used to run Rendom Forest model on the data
 
@@ -8,10 +9,15 @@ Date: 2024-03-06
 #---import the required libraries---
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+
 
 #---load the data---
 from RunPipe import X_train, X_test, y_train, y_test
 
 #---create the model---
-model = RandomForestClassifier(n_estimators=100, random_state=0)
+# Instantiate model with 1000 decision trees
+rf = RandomForestRegressor(n_estimators = 10, random_state = 42)
+
+# Train the model on training data
+rf.fit(X_train, y_train)
