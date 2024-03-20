3fdf47eca079e3f1cab30b6e07b515e1e6617cf9
barc <barcooooo>
2024-03-20 16:51:16 +0200

    אוקי

--- pyScripts/BarModels/__main__.py
+++ pyScripts/BarModels/__main__.py
@@ -2,6 +2,8 @@
 """
 Main file for the BarModels directory
 """
+%load_ext autoreload
+%autoreload 2
 
 #---------------------------- Imports -------------------------------
 import numpy as np
@@ -17,3 +19,22 @@ y_train = np.load("./y_train.npy")
 #create a Rendom_forest_classification_BC object
 from Rendom_forest import Rendom_forest_classification_BC
 rf = Rendom_forest_classification_BC(X_train_np, y_train, X_train_np, y_train)
+#build the model
+classifier_fit = rf.build_RandomForestClassifier()
+#predict the model
+predictions = rf.predict_RandomForestClassifierTrainData(classifier_fit)
+#check the accuracy
+#%%
+accuracy = rf.accuracy_score(predictions)
+print(accuracy)
+predictions = rf.predict_RandomForestClassifierTestData(classifier_fit)
+print(predictions)
+
+#%%
+#cv
+
+
+
+grid= rf.gridSearchCV_RandomForestClassifier()
+
+print(grid.best_params_)
--- pyScripts/main.py
+++ pyScripts/main.py
@@ -1,40 +1,45 @@
-#%%
+
 """"
 This is the fucking main file, all file should be called from here
 """
 # ---------------------------- Imports -------------------------------
 #from RunPipe import * #get all the data from the RunPiple.py file 
 #data such as x train, yratin, xtset ect
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 from AddRootDirectoriesToSysPath import AddRootDirectoriesToSys #importing the function that add all path in the dir to sys
 AddRootDirectoriesToSys() #implement this function
 import pandas as pd
 from deadendscript.feature_importance_gans_4_numeric import *
 from DefPipeLineClasses import DropColumns
+from RunPipe import X_test
 
 # --------------------- Set up the X and y Train and Test value -----------------------
-#call the X_train from csv name copula_train_set.csv
+#call the DFS from the path
 X_train = pd.read_csv('../data/copula_train_set_300_epochs_4_numeric.csv')
-#extract the lables
-y_train = X_train['readmitted']
-#remove labales from X_train
-X_train = X_train.drop('readmitted', axis=1)
-#drop the columns:
-X_train = DropColumns(cols_to_drop).fit_transform(X_train)
-#Change 2 columns to float64 (for pipline purposes) (54428 rows × 30 columns)
-X_train = convert_to_float64(X_train, cols_to_change)
-#fit the data: (54428x119)
-X_train_np = col_processor.fit_transform(X_train)
-
+X_test = pd.read_csv('../data/copula_gans_4_numeric_TestSet.csv')
+#function time
+def FunctionToGet_Y_lables_fromTrainAndTest(X_train, X_test):
+    # make the lable var as y_train and y_test
+    y_train = X_train['readmitted']
+    y_test = X_test['readmitted']
+    #remove the lables from the train and test set
+    X_train = X_train.drop('readmitted', axis=1)
+    X_test = X_test.drop('readmitted', axis=1)
+    #apply the drop columns function to the train and test set
+    X_train = DropColumns(cols_to_drop).fit_transform(X_train)
+    X_test = DropColumns(cols_to_drop).fit_transform(X_test)
+    #convert the columns to float64
+    X_train = convert_to_float64(X_train, cols_to_change)
+    X_test = convert_to_float64(X_test, cols_to_change)
+    #apply the col processor to the train and test set
+    X_train_np = col_processor.fit_transform(X_train)
+    X_test_np = col_processor.fit_transform(X_test)
+    #return all
+    return X_train_np, X_test_np, y_train, y_test
+# call the function
+X_train_np, X_test_np, y_train, y_test = FunctionToGet_Y_lables_fromTrainAndTest(X_train, X_test)
 
 #%%
-import os
-if not os.getcwd() == "/home/labs/cssagi/barc/FGS_ML/ML_Project/pyScripts":
-    os.chdir("/home/labs/cssagi/barc/FGS_ML/ML_Project/pyScripts")
-#%%
-dir_to_save= "./BarModels"
-
-#save the X_train_np matrix to a file
-import numpy as np
-np.save(dir_to_save + "/X_train_np", X_train_np)
-#save the y_train matrix to a file
-np.save(dir_to_save + "/y_train", y_train)
+# from BarModels.GetXYstes import save_data
+# save_data(X_train_np, y_train, X_test_np, y_test, dir_to_save= "./BarModels")
