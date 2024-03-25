0ae399496a168af27126b72eb03b4d31383b0166
guyil <guyilan1gu@gmail.com>
2024-03-25 19:15:48 +0200

    dd

--- pyScripts/GuyTrain/guy_train_xgboost.py
+++ pyScripts/GuyTrain/guy_train_xgboost.py
@@ -1,49 +1,103 @@
-#%%
+
 import pandas as pd
 import numpy as np
 import xgboost as xgb
 import os
 from scipy.sparse import csr_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+#set wd to be pyScripts:
+#os.chdir(os.path.join(os.getcwd(), 'pyScripts'))
 
 path = os.path.join(os.getcwd())
 
-#load all the npy files
-X_train = np.load(path + '/X_train_np.npy', allow_pickle=True).item()
-y_train = np.load(path + '/y_train.npy', allow_pickle=True)
-X_test = np.load(path + '/X_test_np.npy', allow_pickle=True).item()
-y_test = np.load(path + '/y_test.npy',  allow_pickle=True)
+#load the data:
+X_train = pd.read_csv(os.path.join(path, 'X_train_df.csv'))
+X_test = pd.read_csv(os.path.join(path, 'X_test_df.csv'))
+y_train = pd.read_csv(os.path.join(path, 'y_train.csv'))
+y_test = pd.read_csv(os.path.join(path, 'y_test.csv'))
 
-
-
-#%%
+#convert the train and test set to DMatrix:
 dtrain = xgb.DMatrix(X_train, label = y_train)
+dtest = xgb.DMatrix(X_test, label = y_test)
 
+#save the DMatrix to a file:
+dtrain.save_binary('dtrain.buffer')
+dtest.save_binary('dtest.buffer')
 
+#load all the npy files
+#dtest = xgb.DMatrix('dtest.buffer')
+#dtrain = xgb.DMatrix('dtrain.buffer')
 
-#%%
-#Define scoring methods:
+evals_result = {}
 
+# Its optimal value highly depends on the other parameters, 
+#and thus it should be re-tuned each time you update a parameter. 
+# Number of boosting rounds to try
+num_boost_round = 1000
 
+# List of random seeds to use
+seeds = range(15)  # Using 15 seeds
 
-for i in range(2):
-    #Set random seed:
-    seed = i*42
+testsize = 1/len(seeds)
+
+score_tree_table = pd.DataFrame(columns = ['seed', 'best_logloss', 'best_num_trees'])
+
+for seed in seeds:
+
+    print(f"\nTesting with random seed: {seed}")
+
+    testsize = len(seeds)
+
+    #Splitting data into train and validation set:
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size = testsize, random_state = seed)
+
+    #convert the train and validation set to DMatrix:
+    dtrain = xgb.DMatrix(X_train, label = y_train)
+    dval = xgb.DMatrix(X_val, label = y_val)
+
+    evals = [(dtrain, 'train'), (dval, 'eval')]
+
+    # Set random seed using NumPy
     np.random.seed(seed)
+
+    params = {
+    'objective': 'reg:logistic',
+    'eval_metric': 'logloss',
+    'eta': 0.1}
+
+    # Create a watchlist for early stopping
+    watchlist = [(dtrain, 'train'), (dval, 'eval')]
+
+    # Train the model
+    model = xgb.train(params=params,
+                      dtrain=dtrain,
+                      num_boost_round=num_boost_round,
+                      evals=watchlist,
+                      early_stopping_rounds=100,
+                      verbose_eval=True
+                      ) 
     
-    #Define xbg classifier
-    xgb_clf = XGBClassifier(random_state=seed)
+    best_iteration = model.best_iteration
+    best_logloss = model.best_score
 
-    #Early stopping section, 1st try:
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=seed)
+    # Update the score_tree_table
+    new_row = {'seed': seed, 'best_logloss': best_logloss, 'best_num_trees': best_iteration + 1}
+    score_tree_table = pd.concat([score_tree_table, pd.DataFrame([new_row])], ignore_index=True)
 
-    model = XGBClassifier(booster='gbtree', objective='binary:logistic', random_state=2)
+    print("Best logloss: {:.2f} with {} rounds".format(
+                 model.best_score,
+                 model.best_iteration+1))
+    
+#Export score_tree_table to csv
+score_tree_table.to_csv(os.path.join(path, 'score_tree_table.csv'))
 
+#save the model:
+model.save_model(os.path.join(path, 'xgboost.model'))
     
 
-
-
-
-
+    
     
 
 
--- pyScripts/main.py
+++ pyScripts/main.py
@@ -13,7 +13,7 @@ import pandas as pd
 from deadendscript.feature_importance_gans_4_numeric import *
 from DefPipeLineClasses import DropColumns
 from RunPipe import X_test
-
+#%%
 # --------------------- Set up the X and y Train and Test value -----------------------
 #call the DFS from the path
 X_train = pd.read_csv('../data/copula_train_set_300_epochs_4_numeric.csv')
@@ -43,3 +43,47 @@ X_train_np, X_test_np, y_train, y_test = FunctionToGet_Y_lables_fromTrainAndTest
 #%%
 # from BarModels.GetXYstes import save_data
 # save_data(X_train_np, y_train, X_test_np, y_test, dir_to_save= "./BarModels")
+
+#%%
+# --------------------- Makeing DMatrix file for XGBOOST -----------------------
+"""
+
+X_train = pd.read_csv('../data/copula_train_set_300_epochs_4_numeric.csv')
+X_test = pd.read_csv('../data/copula_gans_4_numeric_TestSet.csv')
+
+y_train = X_train['readmitted']
+y_test = X_test['readmitted']
+
+X_train = X_train.drop('readmitted', axis=1)
+X_test = X_test.drop('readmitted', axis=1)
+
+X_train = DropColumns(cols_to_drop).fit_transform(X_train)
+X_test = DropColumns(cols_to_drop).fit_transform(X_test)
+
+X_train = convert_to_float64(X_train, cols_to_change)
+X_test = convert_to_float64(X_test, cols_to_change)
+
+X_train_np = col_processor.fit_transform(X_train)
+
+names_train = col_processor.get_feature_names_out()
+
+feature_names_train = [name[12:] for name in names_train]
+
+X_train_df = pd.DataFrame.sparse.from_spmatrix(X_train_np, columns=feature_names_train)
+
+#do the same for the test set:
+X_test_np = col_processor.fit_transform(X_test)
+
+names_test = col_processor.get_feature_names_out()
+
+feature_names_test = [name[12:] for name in names_test]
+
+X_test_df = pd.DataFrame.sparse.from_spmatrix(X_test_np, columns=feature_names_test)
+
+
+#export X_train_df and X_test_df and y_train and y_test to csv files:
+X_train_df.to_csv('GuyTrain/X_train_df.csv', index=False)
+X_test_df.to_csv('GuyTrain/X_test_df.csv', index=False)
+y_train.to_csv('GuyTrain/y_train.csv', index=False)
+y_test.to_csv('GuyTrain/y_test.csv', index=False)
+"""
