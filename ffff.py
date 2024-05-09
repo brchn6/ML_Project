75c211b3b7014357a1815f81fdb128cbd9ef2823
barc <barc@access4.wexac.weizmann.ac.il>
2024-05-09 10:54:45 +0300

    vff

--- ffff.py
+++
@@ -1,6 +0,0 @@
-import os
-
-here = os.path.dirname(os.path.realpath(__file__))
-if not os.path.exists(here + '/ML_Project/pyScripts/BarModels/logs/logsRF_Main_Run_FullScript'):
-    os.makedirs(here + '/ML_Project/pyScripts/BarModels/logs/logsRF_Main_Run_FullScript')
-    print('Directory created')
